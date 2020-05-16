dd2367a2b2e3486dae00bbf18eb3456b74d44da6
Patrick <patrick204nqh@gmail.com>
2020-05-16 11:43:01 +0700

    add check fields register

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,31 @@ router.post('/register', (req, res) => {
   let errors = [];
 
   // Check require fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please fill in all fields' });
+  }
+
+  // Check passwords match
+  if (password !== password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+
+  // Check pass length
+  if (password.length < 6) {
+    errors.push({ msg: 'Password should be at least 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    res.send('pass');
+  }
 });
 
 module.exports = router;
