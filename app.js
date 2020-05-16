b7e681d0c36a3231fa99e13789b60dfd82dc946b
Patrick <patrick204nqh@gmail.com>
2020-05-16 12:56:44 +0700

    checking newUser before add to database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/User');
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -36,7 +40,42 @@ router.post('/register', (req, res) => {
       password2,
     });
   } else {
-    res.send('pass');
+    // Validation passed
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        // User exists
+        errors.push({ msg: 'Email is already registered' });
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2,
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+
+        // Hash Password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            // Set password to hashed
+            newUser.password = hash;
+            // Save user
+            newUser
+              .save()
+              .then((user) => {
+                res.redirect('/users/login');
+              })
+              .catch((err) => console.log(err));
+          })
+        );
+      }
+    });
   }
 });
 
